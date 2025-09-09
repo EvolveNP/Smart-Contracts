c7525f1dec75a44b3af51b3761667c4ae6962c7f
Addis Belete Bejiga <addis@evolve-np.com>
2025-09-09 18:42:25 +0300

    feat: add FundraisingToken

---
+++ test/FundRaisingToken.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {FundRaisingToken} from "../src/FundRaisingToken.sol";
+
+contract FundRaisingTokenTest is Test {
+    FundRaisingToken public fundRaisingToken;
+
+    function setUp() public {
+        fundRaisingToken = new FundRaisingToken();
+    }
+}
