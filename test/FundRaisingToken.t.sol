7a3cc06f0155d112d4e8d7a0e6f3806822b52bd0
Addis Belete Bejiga <addis@evolve-np.com>
2025-09-12 15:29:34 +0300

    test: tax not incured on mint and burn

--- test/FundRaisingToken.t.sol
+++ test/FundRaisingToken.t.sol
@@ -93,6 +93,18 @@ contract FundRaisingTokenTest is Test {
         assertEq(fundRaisingToken.balanceOf(treasuryAddress), initialTreasuryBalance - burnAmount);
     }
 
+    function testBurnCannotIncurTax() public {
+        uint256 initialTotalSupply = fundRaisingToken.totalSupply();
+        uint256 initialTreasuryBalance = fundRaisingToken.balanceOf(treasuryAddress);
+        uint256 burnAmount = 1e18;
+
+        vm.prank(treasuryAddress);
+        fundRaisingToken.burn(burnAmount);
+
+        assertEq(fundRaisingToken.totalSupply(), initialTotalSupply - burnAmount);
+        assertEq(fundRaisingToken.balanceOf(treasuryAddress), initialTreasuryBalance - burnAmount);
+    }
+
     function testSetLPAddressRevertsIfNotOwner() public {
         vm.prank(address(0x4));
         vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, 0x4));
