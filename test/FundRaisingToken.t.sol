ffe2f75bf9004e6793f8bf5257ebb3531bf889cc
Addis Belete Bejiga <addis@evolve-np.com>
2025-09-12 15:15:08 +0300

    test: add more test cases

--- test/FundRaisingToken.t.sol
+++ test/FundRaisingToken.t.sol
@@ -57,4 +57,49 @@ contract FundRaisingTokenTest is Test {
         assertEq(fundRaisingToken.treasuryAddress(), treasuryAddress);
         assertEq(fundRaisingToken.donationAddress(), donationAddress);
     }
+
+    function testConstructorSetTotalSupplyCorrectly() public {
+        fundRaisingToken =
+            new FundRaisingToken("FundRaisingToken", "FRT", lpManager, treasuryAddress, donationAddress, 5e24);
+        assertEq(fundRaisingToken.totalSupply(), 5e24);
+    }
+
+    function testOwnerIsDeployer() public view {
+        assertEq(fundRaisingToken.owner(), address(this));
+    }
+
+    function testBurnRevertsOnZeroAmount() public {
+        vm.prank(treasuryAddress);
+        vm.expectRevert(bytes("Zero amount"));
+        fundRaisingToken.burn(0);
+    }
+
+    function testBurnRevertsIfNotCalledByTreasury() public {
+        vm.prank(address(0x4));
+        vm.expectRevert(bytes("Only treasury"));
+        fundRaisingToken.burn(1e18);
+    }
+
+    function testBurnReducesTotalSupplyAndTreasuryBalance() public {
+        uint256 initialTotalSupply = fundRaisingToken.totalSupply();
+        uint256 initialTreasuryBalance = fundRaisingToken.balanceOf(treasuryAddress);
+        uint256 burnAmount = 1e18;
+
+        vm.prank(treasuryAddress);
+        fundRaisingToken.burn(burnAmount);
+
+        assertEq(fundRaisingToken.totalSupply(), initialTotalSupply - burnAmount);
+        assertEq(fundRaisingToken.balanceOf(treasuryAddress), initialTreasuryBalance - burnAmount);
+    }
+
+    function testSetLPAddressRevertsOnZeroAddress() public {
+        vm.expectRevert(bytes("Zero address"));
+        fundRaisingToken.setLPAddress(address(0));
+    }
+
+    function testSetLPAddressUpdatesLPAddress() public {
+        address newLPAddress = address(0x5);
+        fundRaisingToken.setLPAddress(newLPAddress);
+        assertEq(fundRaisingToken.lpAddress(), newLPAddress);
+    }
 }
