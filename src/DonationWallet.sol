7c56defea2f245c465d6a2dea3d442ebb526c984
Addis Belete Bejiga <addis@evolve-np.com>
2025-09-18 12:39:57 +0300

    feat: transfer asset

--- src/DonationWallet.sol
+++ src/DonationWallet.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
-contract DonationWallet {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DonationWallet {
+    IERC20 public immutable fundraisingTokenAddress; // Address of the FundRaisingToken contract
+    address public immutable owner; // Owner of the DonationWallet
+
+    constructor(address _fundraisingTokenAddress, address _owner) {
+        fundraisingTokenAddress = IERC20(_fundraisingTokenAddress);
+        owner = _owner;
+    }
+
+    function transferAsset() external view {
+        require(fundraisingTokenAddress.balanceOf(address(this)) > 0, "No tokens to transfer");
+    }
+
+    function swapFundraisingToken() external {
+        // Logic to swap FundRaisingToken for ETH and send to treasury
+    }
+}
